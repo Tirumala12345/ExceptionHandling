16603abd0d4d5c27a7e3b5e67278ff02775f10f0
Tirumala <tirumalatamma17@gmail.com>
2024-03-06 11:52:23 +0530

    Exception

--- exception/BookTickets.java
+++ exception/BookTickets.java
@@ -8,9 +8,14 @@ public class BookTickets {
     public int noOfSeats;
     public int remainingSeats;
      public int availableSeats=5;
+    private int ticketPrice=200;
+    private int totalTicketPrice;
+    private int balance;
+    private int remainingBalance;
 
-    public BookTickets(int noOfSeats) {
+    public BookTickets(int noOfSeats,int balance) {
         this.noOfSeats = noOfSeats;
+        this.balance=balance;
     }
 
     public int getNoOfSeats() {
@@ -35,30 +40,52 @@ public class BookTickets {
 
     public int  bookingTicket(String userName,String password) {
          try {
-             if (userName.equals(validUsername) && password.equals(validPassword))  {
+             if (userName.equals(validUsername) && password.equals(validPassword)) {
                  System.out.println("Login Successful");
                  try {
                      if (noOfSeats <= availableSeats) {
                          remainingSeats = availableSeats - noOfSeats;
                          System.out.println("Booked seats: " + noOfSeats);
-                         System.out.println("Available Seats: " + remainingSeats);
+
+                         //public boolean phonePayService( ){
+                             totalTicketPrice=noOfSeats*200;
+                             System.out.println("Total Ticket Price Is : " +totalTicketPrice );
+
+                             try {
+                                 if (totalTicketPrice < balance) {
+                                     System.out.println("Payment Is Successful");
+                                     remainingBalance=balance-totalTicketPrice;
+                                     System.out.println("Remaining Balance after Payment :" +remainingBalance);
+                                 } else {
+                                     System.out.println(ErrorCodes.Insufficient_Balance.getMessage());
+                                 }
+                             } catch (InsufficientBalanceException e) {
+                                 System.out.println(e.getCode() + "  " + e.getMessage());
+                                 //return false;
+                             }
+                        // }
+                         //System.out.println("Available Seats: " + remainingSeats);
                      } else {
                          System.out.println("Requested seats: " + noOfSeats);
                          System.out.println("Available seats: " + availableSeats);
                          //throw new InValidInputException(ErrorCodes.Invalid_Input.getCode(),
                          //ErrorCodes.Invalid_Input.getMessage());
+                         System.out.println(ErrorCodes.Invalid_Input.getMessage());
                      }
-                 }catch (InValidInputException ie){
-                     throw new InValidInputException(ErrorCodes.Invalid_Input.getCode(),
-                             ErrorCodes.Invalid_Input.getMessage());
+                 } catch (InValidInputException ie) {
+                     //throw new InValidInputException(ErrorCodes.Invalid_Input.getCode(),
+                     //      ErrorCodes.Invalid_Input.getMessage());
+                     System.out.println(ie.getMessage());
                  }
-             } else {
+             }
+             else {
                  throw new InvalidCredentialsException(ErrorCodes.Login_Failed.getCode(),
                          ErrorCodes.Login_Failed.getMessage());
              }
          }catch (InvalidCredentialsException ic){
              System.out.println(ic.getCode() +"  "+ic.getMessage());
          }
-        return remainingSeats;
+         //int seat=noOfSeats-availableSeats;
+        return remainingSeats ;
     }
 }
--- exception/ExceptionTest.java
+++ exception/ExceptionTest.java
@@ -2,12 +2,10 @@ package exception;
 
 public class ExceptionTest {
     public static void main(String[] args) {
-//        LoginProcess loginProcess=new LoginProcess();
-//        loginProcess.login("1234 1234","1234");
-        BookTickets bookTickets=new BookTickets(2);
-        System.out.println("Remaining Seats: " +bookTickets.bookingTicket("1234 1234","1234"));
-        TicketPayment pay=new TicketPayment(2,500);
-        pay.payment();
-        pay.phonePayService();
+        BookTickets bookTickets=new BookTickets(4,1000);
+        System.out.println("Remaining Seats : " +bookTickets.bookingTicket("1234 1234","1234"));
+//       TicketPayment pay=new TicketPayment(2,500);
+//       pay.payment();
+//      pay.phonePayService();
     }
 }
